76bad196a99a7c996dd73e4f4b1e4f49a7b0ec4d
HightoCode <ghatearpit@gmail.com>
2021-07-29 07:01:45 +0000

    First commit for the module

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "server1" {
+ami = var.ami
+instance_type = var.i_type
+tags = {
+    Name = upper("Arpit-app-instance")
+}
+}
+
+module "s3" {
+    source = "./modules/s3-module"
+    bucket_name = var.bucket
+    }
